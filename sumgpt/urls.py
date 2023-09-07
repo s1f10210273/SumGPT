faee9c9133308631157c82a40149672ef3941632
s1f10210273 <s1f102102739@iniad.org>
2023-09-07 22:59:08 +0900

    名前の変更

--- sumgpt/urls.py
+++ sumgpt/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from . import views, auth
+from .view import auth
+from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    #サインアップ・ログイン機能
+    #viewフォルダ内のauth.pyでviewを設定
     path("auth/signup", auth.signup, name="signup"),
     path("auth/login", auth.login_view, name="login"),
+
 ]
--- sumgpt/view/auth.py
+++ sumgpt/view/auth.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
-from .forms import UserForm, LoginForm
+from ..forms import UserForm, LoginForm
+
+
+#サインアップ・ログイン機能
+
 
 def signup(request):
     if request.method == 'POST':
