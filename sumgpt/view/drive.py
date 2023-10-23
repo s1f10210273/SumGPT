27ae59feb8bcebd5f29e0b4c7ac71fe042d9626c
s1f102100119 <s1f102100119@iniad.org>
2023-10-23 18:33:32 +0900

    保存するファイル形式を.docsに変更

--- sumgpt/view/drive.py
+++ sumgpt/view/drive.py
@@ -17,12 +17,12 @@ def access_drive():
 #Docsの新規作成保存
 def create_docs(filename, content):
     drive = access_drive()
-    docs = drive.CreateFile({'title': filename})
+    docs = drive.CreateFile({'title': filename, 'mimeType':	"application/vnd.openxmlformats-officedocument.wordprocessingml.document"})
     docs.SetContentString(content)
-    #作成したファイルの情報を表示
-    print(docs)
     #ファイルのアップロード(google driveの最上位のディレクトリに置かれます。)
     docs.Upload()
+    #作成したファイルの情報を表示
+    #print(docs)
 
 #sumをDocsに保存する
 @login_required
@@ -31,7 +31,7 @@ def save_sum_to_docs(request):
         filename = request.POST.get("filename", None)        
         pk = request.POST.get("pk", None)
         sum_data = get_object_or_404(Sum, pk=pk)
-        content = "ユーザー名:{}\n要約文:\n{}\n本文:\n{}\nタイムスタンプ\n{}".format(sum_data.user, sum_data.sum, sum_data.detail, sum_data.timestamp)
+        content = "ユーザー名： {}\n要約文：\n{}\n本文：\n{}\nタイムスタンプ：\n{}".format(sum_data.user, sum_data.sum, sum_data.detail, sum_data.timestamp)
         create_docs(filename, content)
         return render(request, 'sumgpt/sum.html', {'sum_data': sum_data})
     else:
