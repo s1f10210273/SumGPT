c69b821ddd91855beff50ada770d6a1e94e8986d
shuto114 <s1f102103550@iniad.org>
2023-11-08 15:39:04 +0900

    e

--- sumgpt/static/sumgpt/style.js
+++ sumgpt/static/sumgpt/style.js
@@ -1,77 +1,16 @@
-var livePattern = {
-    canvas: null,
-    context: null,
-    cols: 0,
-    rows: 0,
-    colors: [252, 251, 249, 248, 241, 240],
-
-    init: function() {
-        this.canvas = document.getElementById('canvas');
-        this.context = this.canvas.getContext('2d');
-        this.cols = Math.floor(document.body.clientWidth / 24);
-        this.rows = Math.floor(document.body.clientHeight / 24) + 1;
-
-        this.canvas.width = document.body.clientWidth;
-        this.canvas.height = document.body.clientHeight;
-
-        this.drawBackground();
-        this.animate();
-    },
-
-    drawTriangle: function(x, y, color, inverted) {
-        inverted = inverted === undefined ? false : inverted;
-
-        this.context.beginPath();
-        this.context.moveTo(x, y);
-        this.context.lineTo(inverted ? x - 22 : x + 22, y + 11);
-        this.context.lineTo(x, y + 22);
-        this.context.fillStyle = "rgb(" + color + "," + color + "," + color + ")";
-        this.context.fill();
-        this.context.closePath();
-    },
-
-    getColor: function() {
-        return this.colors[Math.floor(Math.random() * 6)];
-    },
-
-    drawBackground: function() {
-        var eq = null;
-        var x = this.cols;
-        var y;
-        var destY;
-
-        while (x--) {
-            eq = x % 2;
-            y = this.rows;
-
-            while (y--) {
-                destY = Math.round((y - 0.5) * 24);
-
-                this.drawTriangle(x * 24 + 2, eq === 1 ? destY : y * 24, this.getColor());
-                this.drawTriangle(x * 24, eq === 1 ? destY : y * 24, this.getColor(), true);
-            }
-        }
-    },
-
-    animate: function() {
-        var me = this;
-
-        var x = Math.floor(Math.random() * this.cols);
-        var y = Math.floor(Math.random() * this.rows);
-        var eq = x % 2;
-
-        if (eq === 1) {
-            me.drawTriangle(x * 24, Math.round((y - 0.5) * 24), this.getColor(), true);
-        } else {
-            me.drawTriangle(x * 24 + 2, y * 24, this.getColor());
-        }
-
-        setTimeout(function() {
-            me.animate.call(me);
-        }, 10);
-    },
-};
-
-(function() {
-    livePattern.init();
-})();
+//by @nodws
+/*
+Using trianglify.js and d3.js library
+Also not possible without Tim Berners Lee creator of the www, Brendan Eich creator of javascript, Michael S. Dell founder of Dell Computer who manufactured my PC, my Mom and Dad who manufactured me and The BigBang creator of the universe.
+Hope these credits are enough to satisfy snotty commentators
+*/
+//Click to change bg
+var rn = Math.floor((Math.random() * 150) + 60);
+var rs = Math.floor((Math.random() * 11) + 4);
+	var t = new Trianglify({
+ x_gradient: Trianglify.colorbrewer.Spectral[rs],
+    noiseIntensity: 0,
+    cellsize: rn
+});
+var pattern = t.generate(window.innerWidth, window.innerWidth+200);
+document.body.setAttribute('style', 'background-image: '+pattern.dataUrl);
