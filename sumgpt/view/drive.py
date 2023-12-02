c33f7ae6fdf6c040025dc282b1056400ce7a525c
s1f102100119 <s1f102100119@iniad.org>
2023-12-02 14:45:00 +0900

    wordファイルに書き出し、Driveにアップロードする機能追加

--- sumgpt/view/drive.py
+++ sumgpt/view/drive.py
@@ -15,17 +15,23 @@ def access_drive():
     return GoogleDrive(gauth)
 
 #Docsの新規作成保存
-def create_docs(filename, content):
+def create_docs(filename):
     try:
         drive = access_drive()
-        docs = drive.CreateFile({'title': filename, 'mimeType':	"text/plain"})
-        docs.SetContentString(content)
+        docs = drive.CreateFile()
+        docs.SetContentFile(get_wordfile_path(filename))
         #ファイルのアップロード(google driveの最上位のディレクトリに置かれます。)
         docs.Upload(param={'convert': True})
         #作成したファイルのURLを返す
         return 'https://docs.google.com/document/d/' + str(docs['id'])
     except:
         return "ERROR"
+
+def get_wordfile_path(filename):
+    current_dir_path = os.path.dirname(os.path.abspath(__file__))
+    settings_file_path = os.path.join(current_dir_path,'../pydrive/{}.docx'.format(filename))
+    return settings_file_path
+
 
 #wordファイルの作成関数
 def create_wordfile(sum:Sum, filename):
@@ -40,17 +46,14 @@ def create_wordfile(sum:Sum, filename):
         if(doc.paragraphs[i].runs[0].bold): continue
         for k, v in replace_dic.items():
             doc.paragraphs[i].text =  doc.paragraphs[i].text.replace(k, str(v).replace("\r", ""))
-    current_dir_path = os.path.dirname(os.path.abspath(__file__))
-    settings_file_path = os.path.join(current_dir_path,'../pydrive/{}.docx'.format(filename))
+    settings_file_path = get_wordfile_path(filename)
     doc.save(settings_file_path)
 
 #sumをDocsに保存する
 def save_sum_to_docs(filename, pk):
     sum = get_object_or_404(Sum, pk=pk)
     create_wordfile(sum, filename)
-    return {"msg":"", "url": ""}
-    content = "ユーザー名： {}\n要約文：\n{}\n本文：\n{}\nタイムスタンプ：\n{}".format(sum.user, sum.sum, sum.detail, sum.timestamp)
-    docs_url = create_docs(filename, content)
+    docs_url = create_docs(filename)
     drive_msg = {"msg":"", "url": ""}
     if (docs_url == "ERROR"):
         drive_msg["msg"] = "GoogleDriveへの保存に失敗しました。"
@@ -59,6 +62,7 @@ def save_sum_to_docs(filename, pk):
         drive_msg["url"] = docs_url
         # DBの保存 
         Doc.objects.update_or_create(sum=sum, defaults={"url":docs_url})
+    os.remove(get_wordfile_path(filename))
     return drive_msg
 
 
