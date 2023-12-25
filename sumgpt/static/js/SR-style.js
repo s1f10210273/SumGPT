f9a17c3ea9ada7b3796a9ef1cbcdcf553f8db1ec
shuto114 <s1f102103550@iniad.org>
2023-12-26 02:19:45 +0900

    遷移

---
+++ sumgpt/static/js/SR-style.js
@@ -0,0 +1,13 @@
+$(window).on('load',function(){
+    
+    $('body').addClass('appear');//フェードアウト後bodyにappearクラス付与
+  
+    //=====ここまでローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
+    
+   //=====ここから背景が伸びた後に動かしたいJSをまとめたい場合は
+    $('.splashbg').on('animationend', function() {    
+        //この中に動かしたいJSを記載
+    });
+    //=====ここまで背景が伸びた後に動かしたいJSをまとめる
+        
+});
